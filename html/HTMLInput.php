87177e4e49091c0f9a38ed81f39808d8893f17fd
Brent Johnson <johnsonbrentp@gmail.com>
2018-03-27 23:28:26 -0600

    stash current progress
    Building class files for different input types

---
+++ html/HTMLInput.php
@@ -0,0 +1,33 @@
+<?php
+
+class HTMLInput {
+    private $name;
+    private $id;
+    private $type;
+    private $value;
+    private $style;
+    private $border;
+    private $autocomplete;
+    private $autofocus;
+    private $form;
+    private $formaction;
+    private $formenctype;
+    private $formmethod;
+    private $formnovalidate;
+    private $formtarget;
+    private $height;
+    private $width;
+    private $list;
+    private $min;
+    private $max;
+    private $multiple;
+    private $pattern;
+    private $placeholder;
+    private $required;
+    private $step;
+    
+    function __construct($name = null, $id = null) {
+        
+    }
+}
+?>
